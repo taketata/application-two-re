121f98e05a3df31f4862b53fd01f1f615616dc24
EC2 Default User <ec2-user@ip-172-31-10-102.ec2.internal>
2022-03-31 15:09:47 +0000

    4回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,29 @@ class UsersController < ApplicationController
   def index
     @user = current_user
     @users = User.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
     @user = current_user
-    @books = Book.all
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body,:image)
+  end
+
 end
